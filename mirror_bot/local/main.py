0eec8f407761dd93142acd603ec9d7cd1650c4db
Karan Sheth <karan.sheth@nyu.edu>
2022-02-15 12:39:14 -0500

    Mirror Bot Local HW works
    Notes:
    - Some noise in motor of follower due to continuos HW updates

--- mirror_bot/local/main.py
+++ mirror_bot/local/main.py
@@ -1,13 +1,14 @@
 import time
-import socket                                         
+import socket
 import dynamic_graph_manager_cpp_bindings
 from dynamic_graph_head import HoldPDController
 from robot_properties_nyu_finger.config import NYUFingerDoubleConfig0, NYUFingerDoubleConfig1
 from dynamic_graph_head import ThreadHead
 
-from utils import GravityCompensationController
+from utils import GravityCompensationController, MirrorVelocityPositionHWController
 
-def start_hw_thread():
+
+def leader():
     head = dynamic_graph_manager_cpp_bindings.DGMHead(
         NYUFingerDoubleConfig0.dgm_yaml_path)
 
@@ -24,19 +25,33 @@ def start_hw_thread():
     )
     thread_head.start()
     thread_head.switch_controllers([grav_ctrl])
-    
+
+    return head
+
+
+def follower(leader_head):
+    head = dynamic_graph_manager_cpp_bindings.DGMHead(
+        NYUFingerDoubleConfig1.dgm_yaml_path)
+
+    hold_ctrl = HoldPDController(head, 3., 0.05, False)
+    copy_ctrl = MirrorVelocityPositionHWController(
+        head, leader_head)
+
+    thread_head = ThreadHead(
+        0.001,  # dt.
+        [hold_ctrl],  # Safety controllers.
+        {'head': head},  # Heads to read / write from.
+        [],
+    )
+    thread_head.start()
+    thread_head.switch_controllers([copy_ctrl])
+
     return head
 
 
 if __name__ == "__main__":
 
-    # start socket thread
-    # start main program
-    robot_head = start_hw_thread()
-
-    
-    # while True:
-    #     joint_positions = robot_head.get_sensor("joint_positions")
-    #     joint_velocities = robot_head.get_sensor("joint_velocities")
-    #     print(joint_positions, joint_velocities)
-    #     time.sleep(0.001)
+    # start leader program
+    leader_head = leader()
+    # start follower program
+    follower_head = follower(leader_head)
--- mirror_bot/local/utils.py
+++ mirror_bot/local/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pinocchio as pin
 
+
 class GravityCompensationController:
     def __init__(self, head, pin_robot):
         """
@@ -29,3 +30,28 @@ class GravityCompensationController:
         self.head.set_control("ctrl_joint_torques", self.tau)
         return
 
+
+class MirrorVelocityPositionHWController:
+    def __init__(self, head, leader_head):
+        """
+        Args:
+            head: Instance of DGHead or SimHead.
+            leader_head: Instance of DGHead or SimHead
+        """
+        self.head = head
+        self.leader_head = leader_head
+
+        self.des_position = leader_head.get_sensor("joint_positions")
+        self.des_velocity = leader_head.get_sensor("joint_velocities")
+
+    def warmup(self, thread_head):
+        P = np.ones(3)
+        D = np.ones(3)
+        self.head.set_control('ctrl_joint_position_gains', P)
+        self.head.set_control('ctrl_joint_velocity_gains', D)
+        return
+
+    def run(self, thread_head):
+        self.head.set_control("ctrl_joint_positions", self.des_position)
+        self.head.set_control("ctrl_joint_velocities", self.des_velocity)
+        return
