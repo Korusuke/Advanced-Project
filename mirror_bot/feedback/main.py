aa732af7bf2bf6757d2b5db146dfd2d10b4fa40c
Karan Sheth <karan.sheth@nyu.edu>
2022-03-23 11:50:38 -0400

    Basic force feedback controller

---
+++ mirror_bot/feedback/main.py
@@ -0,0 +1,68 @@
+import time
+import socket
+import dynamic_graph_manager_cpp_bindings
+from dynamic_graph_head import HoldPDController
+from robot_properties_nyu_finger.config import NYUFingerDoubleConfig0, NYUFingerDoubleConfig1
+from dynamic_graph_head import ThreadHead
+import numpy as np
+
+from utils import (GravityCompensationControllerWithFeedback,
+                   MirrorHeadGravityControllerForceFeedback)
+
+
+shared_vars = {'leader_pos': np.array([0., 0., 0.]), 'leader_vel': np.array([0., 0., 0.]),
+               'follower_pos': np.array([0., 0., 0.]), 'follower_vel': np.array([0., 0., 0.])}
+
+
+def leader():
+    global shared_vars
+    head = dynamic_graph_manager_cpp_bindings.DGMHead(
+        NYUFingerDoubleConfig0.dgm_yaml_path)
+
+    pin_robot = NYUFingerDoubleConfig0.pin_robot
+
+    hold_ctrl = HoldPDController(head, 3., 0.05, False)
+    grav_ctrl = GravityCompensationControllerWithFeedback(
+        head, pin_robot, shared_vars)
+
+    thread_head = ThreadHead(
+        0.001,  # dt.
+        [hold_ctrl],  # Safety controllers.
+        {'head': head},  # Heads to read / write from.
+        [],
+    )
+    thread_head.start()
+    thread_head.switch_controllers([grav_ctrl])
+
+    return head
+
+
+def follower():
+    global shared_vars
+    head = dynamic_graph_manager_cpp_bindings.DGMHead(
+        NYUFingerDoubleConfig1.dgm_yaml_path)
+
+    pin_robot = NYUFingerDoubleConfig1.pin_robot
+
+    hold_ctrl = HoldPDController(head, 3., 0.05, False)
+    copy_ctrl = MirrorHeadGravityControllerForceFeedback(
+        head, pin_robot, shared_vars)
+
+    thread_head = ThreadHead(
+        0.001,  # dt.
+        [hold_ctrl],  # Safety controllers.
+        {'head': head},  # Heads to read / write from.
+        [],
+    )
+    thread_head.start()
+    thread_head.switch_controllers([copy_ctrl])
+
+    return head
+
+
+if __name__ == "__main__":
+
+    # start leader program
+    leader_head = leader()
+    # start follower program
+    follower_head = follower()
---
+++ mirror_bot/feedback/utils.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pinocchio as pin
+
+
+class GravityCompensationControllerWithFeedback:
+    def __init__(self, head, pin_robot, shared_vars):
+        """
+        Args:
+            head: Instance of DGHead or SimHead
+            pin_robot: Pinocchio robot instance
+            shared_vars: Dictonary containing all the shared variables
+        """
+        self.head = head
+        self.shared_vars = shared_vars
+        self.joint_positions = head.get_sensor("joint_positions")
+        self.joint_velocities = head.get_sensor("joint_velocities")
+
+        shared_vars['leader_pos'] = self.joint_positions
+        shared_vars['leader_vel'] = self.joint_velocities
+
+        self.follower_pos = shared_vars['follower_pos']
+
+        self.zeros_pos = np.zeros_like(self.joint_positions)
+        self.robot = pin_robot
+
+    def warmup(self, thread_head):
+        self.base_pos = self.joint_positions.copy()
+        return
+
+    def rnea(self, q, dq, ddq):
+        return pin.rnea(self.robot.model, self.robot.data, q, dq, ddq)
+
+    def run(self, thread_head):
+        self.tau = self.rnea(self.joint_positions,
+                             self.zeros_pos, self.zeros_pos)
+        # EXPERIMENTAL
+        diff = self.shared_vars['follower_pos'] - \
+            self.shared_vars['leader_pos']
+        P = 1 * np.ones(3)
+
+        self.tau += (
+            P * diff
+        )
+        # END OF EXPERIMENTAL
+
+        self.head.set_control("ctrl_joint_torques", self.tau)
+        return
+
+
+class MirrorHeadGravityControllerForceFeedback:
+    def __init__(self, head, pin_robot, shared_vars):
+        """
+        Args:
+            head: Instance of DGHead or SimHead
+            pin_robot: Pinocchio robot instance
+            shared_vars: Dictonary containing all the shared variables
+        """
+        self.head = head
+        self.shared_vars = shared_vars
+
+        self.des_position = shared_vars.get("leader_pos")
+        self.des_velocity = shared_vars.get("leader_vel")
+
+        self.joint_positions = head.get_sensor("joint_positions")
+        self.joint_velocities = head.get_sensor("joint_velocities")
+
+        shared_vars['follower_pos'] = self.joint_positions
+        shared_vars['follower_vel'] = self.joint_velocities
+
+        self.zeros_pos = np.zeros_like(self.joint_positions)
+        self.robot = pin_robot
+
+    def warmup(self, thread_head):
+        return
+
+    def rnea(self, q, dq, ddq):
+        return pin.rnea(self.robot.model, self.robot.data, q, dq, ddq)
+
+    def run(self, thread_head):
+        P = 3 * np.ones(3)
+        D = 0.05 * np.ones(3)
+
+        self.tau = (
+            P * (self.des_position - self.joint_positions)
+            + D * (self.des_velocity - self.joint_velocities)
+        )
+        self.tau += self.rnea(self.des_position,
+                              self.zeros_pos, self.zeros_pos)
+        self.head.set_control("ctrl_joint_torques", self.tau)
+        return
